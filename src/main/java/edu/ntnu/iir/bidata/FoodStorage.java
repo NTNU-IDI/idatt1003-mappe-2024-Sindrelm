37cd4096ba66f2926d9e19b791a44fae2d5fe495
Sindre <Sindrelmjoes@gmail.com>
2024-10-31 20:03:19 +0100

    La til JavaDoc i foodstorage klassen

--- src/main/java/edu/ntnu/iir/bidata/FoodStorage.java
+++ src/main/java/edu/ntnu/iir/bidata/FoodStorage.java
@@ -2,14 +2,28 @@ package edu.ntnu.iir.bidata;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a storage for grocery items.
+ */
 public class FoodStorage {
 
   private ArrayList<Grocery> groceries;
 
+  /**
+   * Constructs a new FoodStorage with the specified list of groceries.
+   *
+   * @param groceries the list of groceries to be stored
+   */
   public FoodStorage(ArrayList<Grocery> groceries) {
     this.groceries = groceries;
   }
 
+  /**
+   * Adds a grocery item to the storage. If the grocery item already exists, it adds the specified
+   * amount to the existing item.
+   *
+   * @param grocery the grocery item to be added
+   */
   public void addGrocery(Grocery grocery) {
     if (!groceryExists(grocery.getFoodName())) {
       groceries.add(grocery);
@@ -19,6 +33,12 @@ public class FoodStorage {
     }
   }
 
+  /**
+   * Checks if a grocery item with the specified food name exists in the storage.
+   *
+   * @param foodName the name of the food item to check
+   * @return true if the grocery item exists, false otherwise
+   */
   public boolean groceryExists(String foodName) {
     int i = 0;
     while (i < groceries.size()) {
@@ -30,7 +50,12 @@ public class FoodStorage {
     return false;
   }
 
-
+  /**
+   * Gets the index of the grocery item in the storage.
+   *
+   * @param grocery the grocery item to find
+   * @return the index of the grocery item, or -1 if not found
+   */
   public int getGroceryInd(Grocery grocery) {
     String foodName = grocery.getFoodName();
     int i = 0;
