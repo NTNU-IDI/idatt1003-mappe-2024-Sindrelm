802795e880e3e5cf0530723ddd5f16f6e9e47d5f
Sindre <Sindrelmjoes@gmail.com>
2024-10-31 14:36:33 +0100

    Fikset google checkstyle

--- src/main/java/edu/ntnu/iir/bidata/Grocery.java
+++ src/main/java/edu/ntnu/iir/bidata/Grocery.java
@@ -1,41 +1,49 @@
 package edu.ntnu.iir.bidata;
+
 import java.util.Date;
 
-public class Grocery{
-    //variabler
-    private final String foodName;
-    private final String siUnit;
-    private Double amount;
-    private final Double price;
-    private final Date date;
-    
-    //konstruktør
-    public Grocery(String foodName, String siUnit, double amount, double price, Date date){
-        this.foodName = foodName;
-        this.siUnit = siUnit;
-        this.amount = amount;
-        this.date = date;
-        this.price = price;
+public class Grocery {
+
+  //variabler
+  private final String foodName;
+  private final String siUnit;
+  private Double amount;
+  private final Double price;
+
+
+  private final Date date;
+
+  //konstruktør
+  public Grocery(String foodName, String siUnit, double amount, double price, Date date) {
+    this.foodName = foodName;
+    this.siUnit = siUnit;
+    this.amount = amount;
+    this.date = date;
+    this.price = price;
+  }
+
+  public String getFoodName() {
+    return foodName;
+  }
+
+  public String getUnit() {
+    return siUnit;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean getHoldbarhet() {
+    Date dateToday = new Date();
+    if (date.compareTo(dateToday) > 0) {
+      return true;
+    } else {
+      return false;
     }
-    public String getFoodName(){
-        return foodName;
-    }
-    public String getUnit(){
-        return siUnit;
-    }
-    public double getAmount(){
-        return amount;
-    }
-    public double getPrice(){
-        return price;
-    }    
-    public boolean  getHoldbarhet(){
-        Date dateToday = new Date();
-        if(date.compareTo(dateToday) > 0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+  }
 }
---
+++ src/test/java/edu/ntnu/iir/bidata/GroceryTest.java
@@ -0,0 +1,14 @@
+package edu.ntnu.iir.bidata;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+class GroceryTest {
+
+  @Test
+  void getPrice() {
+    assertFalse(false);
+  }
+}
