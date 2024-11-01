032031122ea88a5478294eba9d5b0f4e7dd9209e
Sindre <Sindrelmjoes@gmail.com>
2024-11-01 13:29:26 +0100

    Fikset litt på JavaDoc

--- src/main/java/edu/ntnu/iir/bidata/FoodStorage.java
+++ src/main/java/edu/ntnu/iir/bidata/FoodStorage.java
@@ -22,13 +22,10 @@ public class FoodStorage {
    * Retrieves a grocery item from the storage by its food name.
    *
    * @param foodName the name of the food item to retrieve
-   * @return the grocery item with the specified food name, or null if not found
+   * @return the grocery item with the specified food name
    */
   public Grocery getGrocery(String foodName) {
     int i = getGroceryInd(foodName);
-    if (i == -1) {
-      return null;
-    }
     return groceries.get(i);
   }
 
@@ -51,15 +48,29 @@ public class FoodStorage {
     }
   }
 
+  /**
+   * Removes a grocery item from the storage by its food name.
+   *
+   * @param foodName the name of the food item to remove
+   * @throws IllegalArgumentException if the grocery item does not exist
+   */
   public void removeGrocery(String foodName) {
     int groceryIndex = getGroceryInd(foodName);
     groceries.remove(groceryIndex);
   }
 
-  public void removeGroceryAmount(String foodName, double Amount) {
+  /**
+   * Removes a specified amount of a grocery item from the storage. If the resulting amount is less
+   * than or equal to zero, the grocery item is removed from the storage.
+   *
+   * @param foodName the name of the food item to remove
+   * @param amount   the amount of the food item to remove
+   * @throws IllegalArgumentException if the grocery item does not exist
+   */
+  public void removeGroceryAmount(String foodName, double amount) {
     if (groceryExists(foodName)) {
       Grocery grocery = getGrocery(foodName);
-      grocery.addAmount(-Amount);
+      grocery.addAmount(-amount);
       if (grocery.getAmount() <= 0) {
         removeGrocery(foodName);
       }
@@ -89,7 +100,8 @@ public class FoodStorage {
    * Gets the index of the grocery item in the storage.
    *
    * @param foodName the grocery item to find
-   * @return the index of the grocery item, or -1 if not found
+   * @return the index of the grocery item
+   * @throws IllegalArgumentException if the grocery item does not exist
    */
   public int getGroceryInd(String foodName) {
     int i = 0;
