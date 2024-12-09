e9304199d803afb5ec8cf5da95380bf69812148a
Sindre <Sindrelmjoes@gmail.com>
2024-12-09 13:43:32 +0100

    Fikset varsler i UI og App

--- src/main/java/edu/ntnu/idi/idatt/App.java
+++ src/main/java/edu/ntnu/idi/idatt/App.java
@@ -1,7 +1,6 @@
 package edu.ntnu.idi.idatt;
 
 import edu.ntnu.idi.idatt.UI.UserInterface;
-import java.text.ParseException;
 
 /**
  * The main application class that initializes and starts the user interface.
@@ -12,9 +11,8 @@ public class App {
    * The main method that serves as the entry point of the application.
    *
    * @param args the command line arguments
-   * @throws ParseException if there is an error during parsing
    */
-  public static void main(String[] args) throws ParseException {
+  public static void main(String[] args) {
     UserInterface userInterface = new UserInterface();
     userInterface.init();
     userInterface.start();
