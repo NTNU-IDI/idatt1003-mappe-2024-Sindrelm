5aaff0056351e38d278f19b71fda85e6cfa2224e
Sindre <Sindrelmjoes@gmail.com>
2024-12-09 13:42:50 +0100

    Fikset varsler i UI

--- src/main/java/edu/ntnu/idi/idatt/UI/UserInterface.java
+++ src/main/java/edu/ntnu/idi/idatt/UI/UserInterface.java
@@ -2,9 +2,7 @@ package edu.ntnu.idi.idatt.UI;
 
 import edu.ntnu.idi.idatt.modules.CookBook;
 import edu.ntnu.idi.idatt.modules.FoodStorage;
-import edu.ntnu.idi.idatt.modules.Grocery;
 import edu.ntnu.idi.idatt.modules.Recipe;
-import java.text.ParseException;
 import java.util.ArrayList;
 
 /**
@@ -20,8 +18,8 @@ public class UserInterface {
    * Initializes the food storage and cookbook with some default values.
    */
   public void init() {
-    foodStorage = new FoodStorage(new ArrayList<Grocery>());
-    cookBook = new CookBook(new ArrayList<Recipe>());
+    foodStorage = new FoodStorage(new ArrayList<>());
+    cookBook = new CookBook(new ArrayList<>());
 
     foodStorage.addGrocery("Milk", "l", 1, 20, "19.11.2020");
     foodStorage.addGrocery("Egg", "kg", 0.6, 50, "28.11.2024");
@@ -38,16 +36,12 @@ public class UserInterface {
     salatOppskrift.addGrocery("Mango", "kg", 0.5, 65);
     salatOppskrift.addGrocery("Salatblader", "kg", 0.5, 29);
     cookBook.addRecipe(salatOppskrift);
-
-    boolean run;
   }
 
   /**
    * Starts the user interface, displaying the main menu and handling user input.
-   *
-   * @throws ParseException if there is an error during parsing
    */
-  public void start() throws ParseException {
+  public void start() {
     init();
     boolean runUserInterface = true;
     while (runUserInterface) {
@@ -77,9 +71,6 @@ public class UserInterface {
         case 2 -> {
           System.out.println("Opening FoodStorage ...");
           FoodStorageView.menu();
-        }
-        default -> {
-          System.out.println("Invalid input");
         }
       }
     }
